dc6de51cf8799ef933d27460f3d4a376206d7667
N. M. Luna <nat.luna.64@gmail.com>
2022-11-16 15:10:35 -0800

    add where is the pointer exercise

--- wheresThePointer.js
+++ wheresThePointer.js
@@ -1,3 +1,18 @@
 // Attach one listener that will detect clicks on any of the <td>
 // elements.  Should update that element's innerHTML to be the
 // x, y coordinates of the mouse at the time of the click
+
+// Access the table.
+const tableElement = document.querySelector('tbody')
+// Add event listener to the whole table. When clicked, 
+tableElement.addEventListener('click', (e) => {
+    // save the coordinates for the position of the mouse,
+    const x = e.clientX
+    const y = e.clientY
+
+    // save the cell where the click happened,
+    const clickedCell = e.target
+
+    // and change the cell's inner HTML to the coordinates
+    clickedCell.innerHTML = `(${x}, ${y})`
+})
