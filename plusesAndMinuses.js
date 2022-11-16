f3e233e0b047389faa6086b8325daac1017cc762
N. M. Luna <nat.luna.64@gmail.com>
2022-11-16 14:55:57 -0800

    add pluses and minuses exercise 2

--- plusesAndMinuses.js
+++ plusesAndMinuses.js
@@ -4,4 +4,10 @@ const counterDisplay = document.querySelector('.counter-display')
 counterDisplay.innerHTML = counter 
 
 // When a user clicks the + element, the count should increase by 1 on screen.
+const plusBtn = document.querySelector('.plus-btn')
+plusBtn.addEventListener('click', () => {
+    counter++
+    counterDisplay.innerHTML = counter 
+})
+
 // When a user clicks the – element, the count should decrease by 1 on screen.
