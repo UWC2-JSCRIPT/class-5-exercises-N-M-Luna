0d6b6c90b7b33e296803b7ed465982b7ba41d466
N. M. Luna <nat.luna.64@gmail.com>
2022-11-16 15:43:38 -0800

    add hw exercise 1

--- toDoList.js
+++ toDoList.js
@@ -1,5 +1,33 @@
 // If an li element is clicked, toggle the class "done" on the <li>
 
+const toDoList = document.querySelector('ul')
+// When anything on the to do list is clicked, 
+toDoList.addEventListener('click', (e) => {
+  // Get the list item from the item that was clicked.
+  const itemClicked = e.target
+  let listItem
+
+  switch (itemClicked.tagName) {
+    // If the click happened on a SPAN or an A tag, 
+    case 'SPAN':
+    case 'A':
+      // the list item is the second element on the event's path.
+      listItem = e.path[1]
+      listItem.classList.toggle('done')
+      break;
+
+    // If the click happened on an LI tag,
+    case 'LI':
+      // the list item is the event's target.
+      listItem = e.target
+      listItem.classList.toggle('done')
+      break;
+  
+    default: // If the client clicks on the ul (but not on any item) do nothing.
+      break;
+  }
+})
+
 // If a delete link is clicked, delete the li element / remove from the DOM
 
 // If an 'Add' link is clicked, adds the item as a new list item with
