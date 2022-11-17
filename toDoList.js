8ce94ced833fb306da68e4b879e8442439836423
N. M. Luna <nat.luna.64@gmail.com>
2022-11-16 16:41:19 -0800

    fix hw exercise 2 and add exercise 3

--- toDoList.js
+++ toDoList.js
@@ -26,16 +26,16 @@ toDoList.addEventListener('click', (e) => {
     default: // If the client clicks on the ul (but not on any item) do nothing.
       break;
   }
-})
+  
+  // If a delete link is clicked, delete the li element / remove from the DOM
 
-// If a delete link is clicked, delete the li element / remove from the DOM
-const deleteBtn = document.querySelector('.delete')
-// When a delete link is clicked, 
-deleteBtn.addEventListener('click', (e) => {
-  //grab the list item,
-  const unwantedItem = e.target.parentNode 
-  // and delete it from the parent node
-  toDoList.removeChild(unwantedItem)
+  // If the client clicked on a .delete element,
+  if (e.target.classList.contains('delete')) {
+    //grab the list item,
+    const unwantedItem = e.target.parentNode 
+    // and delete it from the parent node
+    toDoList.removeChild(unwantedItem)
+  }
 })
 
 // If an 'Add' link is clicked, adds the item as a new list item with
@@ -43,8 +43,29 @@ deleteBtn.addEventListener('click', (e) => {
 // Make sure to add an event listener(s) to your new <li> (if needed)
 const addListItem = function(e) {
   e.preventDefault();
+
+  // Create the elements for the list item:
+  // 1- description of to do item
   const input = this.parentNode.getElementsByTagName('input')[0];
-  const text = input.value; // use this text to create a new <li>
+  const task = input.value; // use this text to create a new <li>
+  const newTask = document.createElement('span')
+  newTask.innerHTML = task
+  // 2- delete button
+  const newDeleteBtn = document.createElement('a')
+  newDeleteBtn.classList.add('delete')
+  newDeleteBtn.innerHTML = 'Delete'
+
+  // Put it all together inside a list item tag
+  const newItem = document.createElement('li')
+  newItem.appendChild(newTask)
+  newItem.appendChild(newDeleteBtn)
+
+  // And add the list item to the end of the unordered list
+  toDoList.appendChild(newItem)
 
   // Finish function here
 };
+
+// When the 'Add' link is clicked, run the AddListItem function
+const addBtn = document.querySelector('.add-item')
+addBtn.addEventListener('click', addListItem)
